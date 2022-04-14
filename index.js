edd9a161f6d2e2b9edc625da1ab917126778bc92
Samuel Reed <samuelreed01@gmail.com>
2022-04-14 15:23:19 +0700

    copy/paste tutorial code

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const app = express();
 
+const PORT = process.env.PORT || 3000;
+
 app.get("/", (req, res)=> {
     console.log("request handled");
     res.send("Hello World")
 })
 
-app.listen(3000)
+app.listen(PORT, ()=>{
+    console.log("app running on port: " + PORT)
+})
